ce46362551df105c3fc6b7e4c902752c722b4e71
Pawel Janowski <pavelrc@gmail.com>
2013-09-04 21:12:26 +0000

    Fixed ext function prefixes.

--- __init__.py
+++ __init__.py
@@ -4,13 +4,11 @@ import sys, os
 import iotbx.pdb
 import argparse
 from scitbx.array_family import flex
-
 #~ sys.path.append('/home/pjanowsk/amberSD/AmberTools/src/xtalutil/Phenix')
 #~ import amber_adaptbx_ext as PAI
-
 import boost.python
 ext = boost.python.import_ext("amber_adaptbx_ext")
-from amber_adaptbx_ext import *
+
 
 class geometry_manager(object):
 
@@ -31,12 +29,12 @@ class geometry_manager(object):
 
   def energies_sites(self):
     #Convert flex arrays to C arrays
-    sites_cart_c=ExtractVec(self.sites_cart.as_double())
-    gradients_c=ExtractVec(self.gradients)
-    target_c=ExtractVec(self.residual_sum)
-    
+    sites_cart_c=ext.ExtractVec(self.sites_cart.as_double())
+    gradients_c=ext.ExtractVec(self.gradients)
+    target_c=ext.ExtractVec(self.residual_sum)
+
     # Call c++ interface to call mdgx to calculate new gradients and target
-    callMdgx(sites_cart_c, gradients_c, target_c, self.prmtop, self.ambcrd)
+    ext.callMdgx(sites_cart_c, gradients_c, target_c, self.prmtop, self.ambcrd)
     # Convert back into python types (eg. into flex arrays for phenix to use)
     self.gradients=flex.vec3_double(gradients_c)
     self.residual_sum= float(target_c[0])
