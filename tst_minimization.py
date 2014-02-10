cf2d9dd4e36b0672d0a659d4f5c33e5695f48cce
Pawel Janowski <pavelrc@gmail.com>
2014-02-10 21:11:59 +0000

    Safer regression test.

--- tst_minimization.py
+++ tst_minimization.py
@@ -7,7 +7,7 @@ import iotbx.pdb
 
 def exercise_vAla3 () :
   pdb_file = libtbx.env.find_in_repositories(
-    relative_path="amber_adaptbx/vAla3/vAla3_minimized.pdb",
+    relative_path="amber_adaptbx/vAla3/vAla3.pdb",
     test=os.path.isfile)
   top_file = libtbx.env.find_in_repositories(
     relative_path="amber_adaptbx/vAla3/vAla3.prmtop",
@@ -16,8 +16,19 @@ def exercise_vAla3 () :
   cif_file = os.path.splitext(top_file)[0] + ".cif"
 
   args = [
-    "phenix.pdbtools",
+    "phenix.geometry_minimization",
     pdb_file,
+    cif_file,
+    "topology_file_name=\"%s\"" % top_file,
+    "coordinate_file_name=\"%s\"" % crd_file,
+    "use_amber=True"
+  ]
+  rc = easy_run.fully_buffered(" ".join(args)).raise_if_errors().return_code
+  assert (rc == 0)
+
+  args = [
+    "phenix.pdbtools",
+    "vAla3_minimized.pdb",
     cif_file,
     "output.file_name=vAla3_shaken.pdb",
     "sites.shake=0.1",
@@ -36,7 +47,7 @@ def exercise_vAla3 () :
   rc = easy_run.fully_buffered(" ".join(args)).raise_if_errors().return_code
   assert (rc == 0)
 
-  pdb_inp = iotbx.pdb.input(file_name=pdb_file)
+  pdb_inp = iotbx.pdb.input(file_name='vAla3_minimized.pdb')
   symm = pdb_inp.crystal_symmetry()
   xray_structure = pdb_inp.xray_structure_simple(enable_scattering_type_unknown=True)
   sites_cart_inp=xray_structure.sites_cart()
