a0e645361f5d26c49e2fd6fb8581a09d54dcbb6d
Pawel Janowski <pavelrc@gmail.com>
2014-07-17 22:59:19 +0000

    Support for multiple ligand types and multiple instances of ligand types.

--- afitt.py
+++ afitt.py
@@ -4,8 +4,10 @@ from cctbx.array_family import flex
 master_phil_str = """
   use_afitt = False
     .type = bool
-  ligand_name = None
-    .type = str  
+  ligand_file = None
+    .type = str
+  ligand_names = None
+    .type = str
   ff = 'mmff'
     .type = str  
   scale = 'gnorm'
@@ -14,94 +16,125 @@ master_phil_str = """
   
 
 class afitt_object:
-  def __init__(self, ligand_path, pdb_hierarchy, ff='mmff', scale='gnorm'):
-    self.n_atoms = 0
-    self.resname = None
-    self.chain = None
-    self.number = 1 
-    self.charge = 0
-    self.partial_charges = None
-    self.atom_elements = None
-    self.bonds = None
-    self.nbonds = 0
-    self.sites_cart_ptrs = None
+  def __init__(self, ligand_path,ligand_names, pdb_hierarchy, 
+                ff='mmff', scale='gnorm'):
+    self.n_atoms = []
+    self.resname = ligand_names
+    self.res_ids = [] #[chain, altloc,resseq]
+    self.charge = []
+    self.partial_charges = []
+    self.atom_elements = []
+    self.bonds = []
+    self.nbonds = []
+    self.sites_cart_ptrs = []
+    self.formal_charges = []
     self.total_model_atoms = 0
     self.ff = ff
-    self.formal_charges = None
     self.scale = scale
     
     cif_object = self.read_cif_file(ligand_path)
     self.process_cif_object(cif_object, pdb_hierarchy)
+    
 
   def read_cif_file(self, ligand_path):
     from iotbx import cif
     cif_object = cif.reader(file_path=ligand_path, strict=False).model()
     return cif_object
     
-  def get_sites_cart_pointers(self, atom_ids, pdb_hierarchy, resname):
+  def get_sites_cart_pointers(self, atom_ids, pdb_hierarchy, chain_id, altloc, resseq):
     sites_cart_ptrs=[0]*len(atom_ids)
+    for model in pdb_hierarchy.models():
+      for chain in model.chains():
+        if chain.id == chain_id:
+          for conformer in chain.conformers():
+            if conformer.altloc == altloc:
+              for residue in conformer.residues():
+                if residue.resseq == resseq:
+                  for atom in residue.atoms():
+                    for atom_id in atom_ids:
+                      if atom.name.strip() == atom_id.strip():
+                        loc=atom_ids.index(atom_id)
+                        sites_cart_ptrs[loc] = atom.i_seq
+    return sites_cart_ptrs
+
+  def get_res_ids(self, pdb_hierarchy, resname):
+    ids=[]
     for model in pdb_hierarchy.models():
       for chain in model.chains():
         for conformer in chain.conformers():
           for residue in conformer.residues():
             if residue.resname == resname:
-              self.chain = chain.id
-              for atom in residue.atoms():
-                for atom_id in atom_ids:
-                  if atom.name.strip() == atom_id.strip():
-                    loc=atom_ids.index(atom_id)
-                    sites_cart_ptrs[loc] = atom.i_seq
-    return sites_cart_ptrs                
+              ids.append([chain.id,conformer.altloc,residue.resseq])
+    return ids
   
   def process_cif_object(self, cif_object, pdb_hierarchy):
-    self.resname = cif_object['comp_list']['_chem_comp.id'][0]
-    comp_rname='comp_%s' %self.resname
-    assert comp_rname == cif_object.keys()[1]
-    self.n_atoms = \
-      int(cif_object['comp_list']['_chem_comp.number_atoms_all'][0])
-    self.partial_charges =  \
-      [float(i) for i in cif_object[comp_rname]['_chem_comp_atom.partial_charge']]
-    self.atom_elements = \
-      [i for i in cif_object[comp_rname]['_chem_comp_atom.type_symbol']] 
-    atom_ids = \
-      [i for i in cif_object[comp_rname]['_chem_comp_atom.atom_id']] 
-    bond_atom_1 = \
-      [atom_ids.index(i) for i in cif_object[comp_rname]['_chem_comp_bond.atom_id_1']] 
-    bond_atom_2 = \
-      [atom_ids.index(i) for i in cif_object[comp_rname]['_chem_comp_bond.atom_id_2']]
-    bond_dict={'single':1, 'double':2, 'triple':3, 'aromatic':4, 'coval':1}         
-    bond_type = \
-      [bond_dict[i] for i in cif_object[comp_rname]['_chem_comp_bond.type']] 
-    self.bonds = zip(bond_atom_1, bond_atom_2, bond_type)
-    self.charge = sum( self.partial_charges )
-    self.nbonds = len(self.bonds)
-    self.sites_cart_ptrs = self.get_sites_cart_pointers(
-          atom_ids, 
-          pdb_hierarchy,
-          self.resname)
+    for res in self.resname:
+      for i, id in enumerate(cif_object['comp_list']['_chem_comp.id']):
+        if res == id:
+          self.n_atoms.append(
+            int(cif_object['comp_list']['_chem_comp.number_atoms_all'][i]) )
+      comp_rname='comp_%s' %res
+      assert cif_object.has_key(comp_rname), "Residue %s not in cif file!" %res
+      self.partial_charges.append(
+        [float(i) for i in cif_object[comp_rname]['_chem_comp_atom.partial_charge']]
+        )
+      self.atom_elements.append(
+        [i for i in cif_object[comp_rname]['_chem_comp_atom.type_symbol']] 
+        )
+      atom_ids = \
+        [i for i in cif_object[comp_rname]['_chem_comp_atom.atom_id']] 
+      bond_atom_1 = \
+        [atom_ids.index(i) for i in cif_object[comp_rname]['_chem_comp_bond.atom_id_1']] 
+      bond_atom_2 = \
+        [atom_ids.index(i) for i in cif_object[comp_rname]['_chem_comp_bond.atom_id_2']]
+      bond_dict={'single':1, 'double':2, 'triple':3, 'aromatic':4, 'coval':1}         
+      bond_type = \
+        [bond_dict[i] for i in cif_object[comp_rname]['_chem_comp_bond.type']] 
+      self.bonds.append( zip(bond_atom_1, bond_atom_2, bond_type) )
+      self.charge.append( sum(self.partial_charges[-1]) )
+      self.nbonds.append ( len(self.bonds[-1]) )
+      res_ids = self.get_res_ids(pdb_hierarchy, res)
+      self.res_ids.append(res_ids)
+      this_res_sites_cart_ptrs=[]
+      for residue_instance in self.res_ids[-1]:
+        this_res_sites_cart_ptrs.append( self.get_sites_cart_pointers(
+                                          atom_ids, 
+                                          pdb_hierarchy,
+                                          chain_id=residue_instance[0],
+                                          altloc=residue_instance[1],
+                                          resseq=residue_instance[2]) 
+                                        )
+      self.sites_cart_ptrs.append( this_res_sites_cart_ptrs )                           
+      if cif_object[comp_rname].has_key('_chem_comp_atom.formal_charge'):
+        self.formal_charges.append(
+          [float(i) for i in cif_object[comp_rname]['_chem_comp_atom.formal_charge']] 
+          )
+      else:
+        self.formal_charges.append([])
     self.total_model_atoms=pdb_hierarchy.atoms_size()
-    try:
-      f_charges=cif_object[comp_rname]['_chem_comp_atom.formal_charge']
-      self.formal_charges = [float(i) for i in f_charges] 
-    except KeyError:
-      pass
     #~ import code; code.interact(local=dict(globals(), **locals()))      
-
-  def make_afitt_input(self, sites_cart, afitt_input):
+    
+  def make_afitt_input(self, sites_cart, afitt_input, resname_i, instance_i):
+    r_i=resname_i
+    i_i=instance_i
+    sites_cart_ptrs=self.sites_cart_ptrs[r_i][i_i]
+    elements=self.atom_elements[r_i]
     f=open(afitt_input,'w')
-    f.write('%d\n' %self.n_atoms)
+    f.write('%d\n' %self.n_atoms[r_i])
     f.write('residue_type %s chain %s number %d total_charge %d\n' 
-            %(self.resname, self.chain, self. number, self.charge))
-    assert len(self.atom_elements) == len(self.sites_cart_ptrs)
-    for atom,ptr in zip(self.atom_elements, self.sites_cart_ptrs):
+            %(self.resname[r_i], self.res_ids[r_i][i_i][0],1,self.charge[r_i] ))
+    assert len(elements) ==  len(sites_cart_ptrs), \
+            "No. of atoms in residue %s, instance %d does not equal to \
+            number of atom seq pointers." %(self.resname[resname_i], instance_i)
+    for atom,ptr in zip(elements, sites_cart_ptrs):
       f.write('%s   %20.16f   %20.16f   %20.16f\n' %(atom, 
             sites_cart[ptr][0], sites_cart[ptr][1], sites_cart[ptr][2]) )
-    f.write('bond_table_nbonds %d\n' %self.nbonds)
-    for bond in self.bonds:
+    f.write('bond_table_nbonds %d\n' %self.nbonds[r_i])
+    for bond in self.bonds[r_i]:
       f.write('%d %d %d\n' %(bond[0], bond[1], bond[2]))
-    if self.formal_charges:
+    if self.formal_charges[r_i]:
       f.write("formal charges\n")
-      for fcharge in formal_charges:
+      for fcharge in self.formal_charges[r_i]:
         f.write ('%d\n' %fcharge)
     f.close()
   
@@ -112,8 +145,11 @@ def call_afitt(afitt_input, afitt_output, ff):
               " >%s" %afitt_output + " 2>trash", shell=True).wait()
   sts = Popen("rm trash", shell=True)
 
-def process_afitt_output(afitt_output, geometry, afitt_object):
-  ptrs = afitt_object.sites_cart_ptrs
+def process_afitt_output(afitt_output, geometry, afitt_object, 
+                          resname_i, instance_i):
+  r_i=resname_i
+  i_i=instance_i                            
+  ptrs = afitt_object.sites_cart_ptrs[r_i][i_i]
   afitt_gradients = flex.vec3_double()
   with open(afitt_output, 'r') as afitt_o:
     for line in afitt_o:
@@ -125,7 +161,9 @@ def process_afitt_output(afitt_output, geometry, afitt_object):
              float(line.split()[2]), 
              float(line.split()[3]) ) ) 
   ### debug_stuff
-  print ("AFITT ENERGY: %10.4f\n" %afitt_energy)
+  print ("AFITT_ENERGY %s_%d: %10.4f\n" 
+                  %(afitt_energy, afitt_object.resname[r_i], 
+                    int(afitt_object.res_ids[i_i][2]) ))
   ### end_debug
   geometry.residual_sum += afitt_energy
   
@@ -146,7 +184,12 @@ def process_afitt_output(afitt_output, geometry, afitt_object):
       phenix_norm = sqrt(phenix_norm)
       afitt_norm = sqrt(afitt_norm)
       gr_scale = phenix_norm/afitt_norm
-      print ("GRNORM RATIO: %10.4f\n" %gr_scale)
+      ### debug_stuff
+      print ("GRNORM_RATIO %s_%d: %10.4f\n" 
+                    %(gr_scale, afitt_object.resname[r_i], 
+                      int(afitt_object.res_ids[i_i][2]) ))
+  
+      ### end_debug
     elif afitt_object.scale == 'noafitt':
       gr_scale = None
     else:
@@ -170,39 +213,46 @@ def process_afitt_output(afitt_output, geometry, afitt_object):
         geometry.gradients[ptr] = scaled_gradient
   return geometry
 
-def get_afitt_energy(cif_file, pdb_hierarchy, ff, sites_cart):
+def get_afitt_energy(cif_file, ligand_names, pdb_hierarchy, ff, sites_cart):
   afitt_o = afitt_object(
                 cif_file,
+                ligand_names,
                 pdb_hierarchy,
                 ff)
   afitt_input='afitt_in'
-  afitt_output='afitt_out' 
-  afitt_o.make_afitt_input(sites_cart=sites_cart, 
-        afitt_input=afitt_input)
-  call_afitt(afitt_input, afitt_output, ff)  
-  with open(afitt_output, 'r') as afitt_o:
-    for line in afitt_o:
-      if line.startswith('ENERGYTAG'):
-        energy=float(line.split()[1])                
-  return energy
+  afitt_output='afitt_out'
+  energies=[]
+  for resname_i,resname in enumerate(afitt_o.resname):
+    for instance_i, instance in enumerate(afitt_o.res_ids[resname_i]):
+      #~ import code; code.interact(local=dict(globals(), **locals())) 
+      afitt_o.make_afitt_input(sites_cart, afitt_input, resname_i, instance_i)
+      call_afitt(afitt_input, afitt_output, ff)  
+      with open(afitt_output, 'r') as afitt_out:
+        for line in afitt_out:
+          if line.startswith('ENERGYTAG'):
+            energy=float(line.split()[1])
+      energies.append([resname, int(instance[2]), energy] )
+  return energies
 
-def run(pdb_file, cif_file, ff='mmff'):
+def run(pdb_file, cif_file, ligand_names, ff='mmff'):
   import iotbx.pdb
   pdb_inp = iotbx.pdb.input(file_name=pdb_file)
   pdb_hierarchy = pdb_inp.construct_hierarchy()
   pdb_hierarchy.atoms().reset_i_seq()
   xrs = pdb_hierarchy.extract_xray_structure()
   sites_cart=xrs.sites_cart()
-  afitt_energy = get_afitt_energy(cif_file, pdb_hierarchy, ff, sites_cart)
-  print "AFITT ENERGY: %10.4f\n" %afitt_energy
+  energies = get_afitt_energy(cif_file, ligand_names, pdb_hierarchy, ff, sites_cart)
+  for energy in energies:
+    print "%s_%d AFITT_ENERGY: %10.4f" %(energy[0], energy[1], energy[2])
  
 if (__name__ == "__main__"):
   import argparse
   parser = argparse.ArgumentParser()
   parser.add_argument("pdb_file", help="pdb file")
   parser.add_argument("cif_file", help="cif file", default=0)
+  parser.add_argument("ligand_names", help="3-letter ligand names separated by commas")
   parser.add_argument("-ff", help="afitt theory: mmff, pm3 or am1", default='mmff')
   args = parser.parse_args()
-  print args.pdb_file, args.cif_file, args.ff
-  run(args.pdb_file, args.cif_file, args.ff)
+  ligand_names=args.ligand_names.split(',')
+  run(args.pdb_file, args.cif_file, ligand_names, args.ff)
 
