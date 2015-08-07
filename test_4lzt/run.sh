7e0ee29fa88b2dbea6900e18b30b2e17c764ad72
Pawel Janowski <pavelrc@gmail.com>
2015-08-07 20:10:15 +0000

    Clean tst_4lzt update readme.

--- test_4lzt/run.sh
+++ test_4lzt/run.sh
@@ -7,7 +7,7 @@ refinement {
   }
   refine {
     strategy = *individual_sites individual_sites_real_space rigid_body \
-               individual_adp group_adp tls occupancies group_anomalous
+               *individual_adp group_adp tls *occupancies group_anomalous
     adp {
       individual {
         anisotropic = "not (element H)"
@@ -16,16 +16,24 @@ refinement {
     }
   }
   bulk_solvent_and_scale {
-    bulk_solvent = False
-    anisotropic_scaling = False
+    bulk_solvent = True
+    anisotropic_scaling = True
     k_sol_b_sol_grid_search = False
     minimization_k_sol_b_sol = False
   }
 }
 EOF
 
-phenix.refine 4lzt.pdb prefix=vs_obs_amber 4lzt-sf-truncated.mtz params.eff \
-  topology_file_name=4lzt.prmtop amber.coordinate_file_name=4lzt.rst7 \
-  use_amber=True \
-  refinement.input.xray_data.r_free_flags.file_name=md_avg_95_rfree.mtz \
-  --overwrite optimize_xyz_weight=False
+phenix.refine \
+	params.eff \
+	4lzt.pdb \
+	4lzt-sf-truncated.mtz \
+	refinement.input.xray_data.r_free_flags.file_name=md_avg_95_rfree.mtz \
+	use_amber=True \
+	topology_file_name=4lzt.prmtop \
+	coordinate_file_name=4lzt.rst7 \
+	md_engine=sander \
+	wxc_scale=0.025 \
+	prefix=vs_obs_amber \
+	--overwrite
+
