ea7452ea502b89e1823c9b503e2c65e8e73c1c59
David A. Case <david.case@rutgers.edu>
2016-09-28 13:59:14 +0000

    minor code simplification

--- interface.py
+++ interface.py
@@ -4,7 +4,6 @@ import numpy as np
 
 def get_amber_struct_object(params):
   amber_params = params.amber
-  import sander, sanderles
   ridingH=True
   if getattr(params, "hydrogens", False): # ensemble refinement does not have this
       if params.hydrogens.refine in ['riding', 'Auto']:
@@ -18,18 +17,13 @@ def get_amber_struct_object(params):
     parm_file_name=amber_params.topology_file_name,
     rst_file_name=amber_params.coordinate_file_name,
     ridingH=ridingH,
-    )
-  if amber_structs.is_LES:
-    sanderles.setup(amber_structs.parm,
+  )
+
+  amber_structs.sander_engine.setup(amber_structs.parm,
            amber_structs.rst.coordinates,
            amber_structs.rst.box,
-           amber_structs.inp)
-  else:
-    sander.setup(amber_structs.parm,
-           amber_structs.rst.coordinates,
-           amber_structs.rst.box,
-           amber_structs.inp)
-  sander=True
+           amber_structs.inp
+  )
 
   if amber_params.order_mapping_file_name is not None:
     order_map_file_name = amber_params.order_mapping_file_name
@@ -37,4 +31,4 @@ def get_amber_struct_object(params):
     mapped_arr = np.loadtxt(order_map_file_name, dtype='i4').transpose()
     amber_structs.order_converter = dict(a2p=mapped_arr[0], p2a=mapped_arr[1])
 
-  return amber_structs, sander
+  return amber_structs, amber_structs.sander_engine
