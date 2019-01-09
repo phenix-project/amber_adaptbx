df02e235e2e1652cbd59b820e3540ecbc59b4ae2
David A. Case <david.case@rutgers.edu>
2019-01-09 19:13:10 +0000

    update side-chain alternate conformer alternatives

--- les_builder/make_addles_input.py
+++ les_builder/make_addles_input.py
@@ -25,7 +25,8 @@ def get_LES_residue_dict(parm):
       resid is 1-based index
   """
   holder = {}
-  holderbb = {}  # flag for backbone alternates
+  holderbb = {}  # flag for N,H,C,O
+  holderca = {}  # flag for CA alternates
   holdercb = {}  # flag for side-chain alternates that include CB
   holdercg = {}  # flag for side-chain alternates that include CG or OG
   holder_atoms = {}  # place to store atom names in this residue that
@@ -37,40 +38,57 @@ def get_LES_residue_dict(parm):
          holder_atoms[atom.residue.idx+1] += " " + atom.name
       else:
          holder_atoms[atom.residue.idx+1] = atom.residue.name + ": " + atom.name
-      if atom.name in ["N", "C", "O" ]:
-        holderbb[atom.residue.idx+1] = 1
+      if atom.name in ["N", "C", "O", "H" ]:
+         holderbb[atom.residue.idx+1] = 1
+      elif atom.name in ["CA" ]:
+        # print "found CA altlocs in residue %d" % int(atom.residue.idx+1)
+        for key in atom.other_locations:
+           cadist = math.sqrt( (atom.xx - atom.other_locations[key].xx)**2 +
+                               (atom.xy - atom.other_locations[key].xy)**2 +
+                               (atom.xz - atom.other_locations[key].xz)**2 )
+           # print "parent: %8.3f %8.3f %8.3f" % ( atom.xx, atom.xy, atom.xz )
+           # print "child : %8.3f %8.3f %8.3f" % ( 
+           #         atom.other_locations[key].xx,
+           #         atom.other_locations[key].xy,
+           #         atom.other_locations[key].xz )
+           # print "distance: %8.3f" % cadist
+           break
+        holder_atoms[atom.residue.idx+1] += "%8.3f" % cadist
+        holderca[atom.residue.idx+1] = cadist
 
       elif atom.name in ["CB" ]:
         # print "found CB altlocs in residue %d" % int(atom.residue.idx+1)
-        # print atom.other_locations.keys() 
-        #  better to loop over keys: following assumes second altloc is 'B'
-        # print "parent:  %8.3f  %8.3f  %8.3f" % ( atom.xx, atom.xy, atom.xz )
-        # print "child :  %8.3f  %8.3f  %8.3f" % ( 
-        #         atom.other_locations["B"].xx,
-        #         atom.other_locations["B"].xy,
-        #         atom.other_locations["B"].xz )
-        cbdist = math.sqrt( (atom.xx - atom.other_locations["B"].xx)**2 +
-                            (atom.xy - atom.other_locations["B"].xy)**2 +
-                            (atom.xz - atom.other_locations["B"].xz)**2 )
-        # print "distance: %8.3f" % cbdist
+        for key in atom.other_locations:
+           cbdist = math.sqrt( (atom.xx - atom.other_locations[key].xx)**2 +
+                               (atom.xy - atom.other_locations[key].xy)**2 +
+                               (atom.xz - atom.other_locations[key].xz)**2 )
+           # print "parent: %8.3f %8.3f %8.3f" % ( atom.xx, atom.xy, atom.xz )
+           # print "child : %8.3f %8.3f %8.3f" % ( 
+           #         atom.other_locations[key].xx,
+           #         atom.other_locations[key].xy,
+           #         atom.other_locations[key].xz )
+           # print "distance: %8.3f" % cbdist
+           break
+        holder_atoms[atom.residue.idx+1] += "%8.3f" % cbdist
         holdercb[atom.residue.idx+1] = cbdist
 
       elif atom.name in ["CG", "OG", "CG1", "CG2" ]:
         # print "found CG altlocs in residue %d" % int(atom.residue.idx+1)
-        # print atom.other_locations.keys() 
-        #  better to loop over keys: following assumes second altloc is 'B'
-        # print "parent:  %8.3f  %8.3f  %8.3f" % ( atom.xx, atom.xy, atom.xz )
-        # print "child :  %8.3f  %8.3f  %8.3f" % ( 
-        #         atom.other_locations["B"].xx,
-        #         atom.other_locations["B"].xy,
-        #         atom.other_locations["B"].xz )
-        cgdist = math.sqrt( (atom.xx - atom.other_locations["B"].xx)**2 +
-                            (atom.xy - atom.other_locations["B"].xy)**2 +
-                            (atom.xz - atom.other_locations["B"].xz)**2 )
-        # print "distance: %8.3f" % cgdist
+        for key in atom.other_locations:
+           cgdist = math.sqrt( (atom.xx - atom.other_locations[key].xx)**2 +
+                               (atom.xy - atom.other_locations[key].xy)**2 +
+                               (atom.xz - atom.other_locations[key].xz)**2 )
+           # print "parent: %8.3f %8.3f %8.3f" % ( atom.xx, atom.xy, atom.xz )
+           # print "child : %8.3f %8.3f %8.3f" % ( 
+           #         atom.other_locations[key].xx,
+           #         atom.other_locations[key].xy,
+           #         atom.other_locations[key].xz )
+           # print "distance: %8.3f" % cgdist
+           break
+        holder_atoms[atom.residue.idx+1] += "%8.3f" % cgdist
         holdercg[atom.residue.idx+1] = cgdist
 
-  return holder, holderbb, holdercb, holdercg, holder_atoms
+  return holder, holderbb, holderca, holdercb, holdercg, holder_atoms
   
 def addles_input(pdb_fn='2igd.pdb', prmtop=None, rst7_file=None):
   root_name = os.path.basename(pdb_fn).split('.')[0]
@@ -107,19 +125,25 @@ def addles_input(pdb_fn='2igd.pdb', prmtop=None, rst7_file=None):
   header = '\n'.join((line.strip() for line in header.split('\n')))
   commands.append(header.strip())
   
-  (holder, holderbb, holdercb, holdercg, holder_atoms) = get_LES_residue_dict(parm)
+  (holder, holderbb, holderca, holdercb, holdercg, holder_atoms) = get_LES_residue_dict(parm)
   
   for chain_id in range(n_asu):
     commands.append('~ protein chain: {}'.format(chain_id+1))
   
     for resid in sorted(holder.keys()):
       n_conformers = holder[resid] + 1
-      if resid in holderbb.keys():
-        line_template = 'spac numc={numc} pick #cca {resid} {resid} done'
+      line_template = ''
+      if resid in holderca.keys():
+        # choose #sia if all CA atoms are within 0.15 Ang, and there are
+        # no backbone alternates:
+        if holderca[resid] > 0.15 or resid in holderbb:
+           line_template = 'spac numc={numc} pick #cca {resid} {resid} done'
+        else:
+           line_template = 'spac numc={numc} pick #sia {resid} {resid} done'
       elif resid in holdercb.keys():
         # choose #sid if all CB atoms are within 0.15 Ang
         if holdercb[resid] > 0.15:
-           line_template = 'spac numc={numc} pick #cca {resid} {resid} done'
+           line_template = 'spac numc={numc} pick #sia {resid} {resid} done'
         else:
            line_template = 'spac numc={numc} pick #sid {resid} {resid} done'
       elif resid in holdercg.keys():
@@ -128,11 +152,13 @@ def addles_input(pdb_fn='2igd.pdb', prmtop=None, rst7_file=None):
            line_template = 'spac numc={numc} pick #sid {resid} {resid} done'
         else:
            line_template = 'spac numc={numc} pick #sig {resid} {resid} done'
-      else:
+      elif not resid in holderbb:
         line_template = 'spac numc={numc} pick #sig {resid} {resid} done'
-      commands.append(line_template.format(numc=n_conformers,
+
+      if line_template:
+         commands.append(line_template.format(numc=n_conformers,
                                            resid=resid+chain_id*n_asu_residues))
-      commands.append("~ " + holder_atoms[resid])
+         commands.append("~ " + holder_atoms[resid])
   
   commands.append("*EOD\n")
   return commands
